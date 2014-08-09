f34e3481f2431f66e766cba29daecf6d58a9154f
Adrien Guéret <76342@supinfo.com>
2014-08-09 15:05:15 +0200

    Remove debug

--- index.php
+++ index.php
@@ -3,18 +3,8 @@
 	require_once 'system/Core.class.php';
 	
 	//Init core
-	//\Eliya\Core::init();
-
-	define('PROJECT_ROOT', __DIR__.DIRECTORY_SEPARATOR);
-
-	$system_path	=	PROJECT_ROOT.'system';
-
-	foreach(new \DirectoryIterator($system_path) as $file)
-	{
-		if( ! $file->isDot() && ! $file->isDir() && $file->getExtension() === 'php')
-			echo '<p>',$file->getPathname(),'</p>';
-	}
+	\Eliya\Core::init();
 
 	//Handle received request
-	//$request	=	new \Eliya\Request($_SERVER['REQUEST_URI']);
-	//$request->exec()->response()->render();
+	$request	=	new \Eliya\Request($_SERVER['REQUEST_URI']);
+	$request->exec()->response()->render();
