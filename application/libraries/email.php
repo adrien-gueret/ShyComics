bd99581af1d50d7c5e381949b31037ca43029424
Tatane58 <antoine.charnotet@hotmail.fr>
2014-08-19 23:25:55 +0200

    Modify email library

--- application/libraries/email.php
+++ application/libraries/email.php
@@ -1,10 +1,10 @@
 <?php
 	abstract class Library_Email
 	{
-		static function send($to, $subject, $message)
+		public static function send($to, $subject, $message)
 		{
 			$expediteur = 'no-reply@shycomics.fr';
-			$headers = 'From: Shy\'Comics <' . $expediteur . '>' . "\r\n" . 'Reply-To: ' . $expediteur . '';
+			$headers = 'From: Shy\'Comics <' . $expediteur . '>';
 			$headers .= "MIME-Version: 1.0\r\n";
 			$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
