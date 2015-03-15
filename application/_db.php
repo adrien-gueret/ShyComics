5430043176379e3c7368c573fba51804dd00bb24
Adrien Guéret <76342@supinfo.com>
2015-03-15 14:28:27 +0100

    Add a script for DB re-initialization

---
+++ application/_db.php
@@ -0,0 +1,42 @@
+<?php
+	// Require framework core
+	require_once '../system/Core.class.php';
+
+	// Init core
+	\Eliya\Core::init();
+
+	// Init DB
+	require_once 'vendors/EntityPHP/EntityPHP.php';
+
+	$sql	=	\Eliya\Config('main')->SQL;
+
+	\EntityPHP\Core::connectToDB($sql['HOST'], $sql['USER'], $sql['PASSWORD'], $sql['DATABASE']);
+
+	// Include all models files
+	\Eliya\Core::requireDirContent('models');
+
+	\EntityPHP\Core::generateDatabase();
+
+	/*== Create data ==*/
+
+	// Locales
+	$locales	=	[
+		new Model_Locales('fr_FR'),
+		new Model_Locales('en_US')
+	];
+	Model_Locales::addMultiple($locales);
+
+	// Users groups
+	$groups	=	[
+		new Model_UsersGroups('Anonyme', [
+			Model_UsersGroups::PERM_REMOVE_OTHERS_FILES	=>	0,
+		]),
+		new Model_UsersGroups('Membre simple', [
+			Model_UsersGroups::PERM_REMOVE_OTHERS_FILES	=>	0,
+		]),
+		new Model_UsersGroups('Admin', [
+			Model_UsersGroups::PERM_REMOVE_OTHERS_FILES	=>	1,
+		]),
+	];
+
+	Model_UsersGroups::addMultiple($groups);
--- application/models/UsersGroups.php
+++ application/models/UsersGroups.php
@@ -5,13 +5,19 @@
 
 		protected $name;
 		/* Permissions */
-		protected $can_remove_other_files;
+		protected $can_remove_other_files = 0;
 
 		const PERM_REMOVE_OTHERS_FILES	=	'can_remove_other_files';
 
-		public function __construct($name = null)
+		public function __construct($name = null, Array $permissions = [])
 		{
 			$this->name = $name;
+
+			foreach($permissions as $name => $value)
+			{
+				if(isset($this->$name))
+					$this->$name	=	$value;
+			}
 		}
 		
 		public static function __structure()
