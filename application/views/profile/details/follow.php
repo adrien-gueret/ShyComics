e95e72fef39dbd60443b5c087016ff5e749aa429
Tatane58 <antoine.charnotet@hotmail.fr>
2016-07-17 23:08:41 +0200

    DELETE and not POST : fixed in views

--- application/views/profile/details/follow.php
+++ application/views/profile/details/follow.php
@@ -1,5 +1,8 @@
 <?php if( ! $view->has_followed): ?>
 	<form action="<?= $view->base_url; ?>profile/follow/<?= $view->user_id; ?>" method="post"><button type="submit" class="orange"><?= Library_i18n::get('profile.follow.buttonFollow'); ?></button></form>
 <?php else: ?>
-	<form action="<?= $view->base_url; ?>profile/follow/unfollow/<?= $view->user_id; ?>" method="post"><button type="submit" class="orange"><?= Library_i18n::get('profile.follow.buttonUnfollow'); ?></button></form>
+	<form action="<?= $view->base_url; ?>profile/follow/unfollow/<?= $view->user_id; ?>" method="delete">
+		<input type="hidden" name="__method__" value="DELETE" />
+		<button type="submit" class="orange"><?= Library_i18n::get('profile.follow.buttonUnfollow'); ?></button>
+	</form>
 <?php endif; ?>
