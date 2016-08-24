03804f894a4b446bcfd2bc7dc0bbabd2d0672d30
Tatane58 <antoine.charnotet@hotmail.fr>
2016-08-24 12:53:34 +0200

    Integrate parser - Profile

--- application/views/profile/form_about.php
+++ application/views/profile/form_about.php
@@ -5,7 +5,9 @@
 	  method="post">
 	<fieldset id="form_about_part">
 		<legend><i class="fa fa-commenting-o" aria-hidden="true"></i></legend>
-		<textarea name="content"><?= $view->user_about; ?></textarea>
+		<textarea id="content-about" name="content"><?= $view->user_about; ?></textarea>
+		<p><?= $view->tpl_buttons; ?></p>
 	</fieldset>
 	<p><button class="orange"><?= Library_i18n::get('profile.submit'); ?></button></p>
 </form>
+<script src="<?= $view->base_url; ?>public/javascript/parser.js"></script>
