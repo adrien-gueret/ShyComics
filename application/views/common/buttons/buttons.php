4e1f9b343c4f6b88509d85eafa7f524d1d6c77da
Tatane58 <antoine.charnotet@hotmail.fr>
2016-08-24 14:50:45 +0200

    Add quotes in parser

--- application/views/common/buttons/buttons.php
+++ application/views/common/buttons/buttons.php
@@ -1,4 +1,5 @@
 <input type="button" value="<?= Library_i18n::get('parser.buttons.bold'); ?>" onclick="insertTag('[b]','[/b]','<?= $view->textareaId; ?>');" />
 <input type="button" value="<?= Library_i18n::get('parser.buttons.italic'); ?>" onclick="insertTag('[i]','[/i]','<?= $view->textareaId; ?>');" />
-<input type="button" value="<?= Library_i18n::get('parser.buttons.underlined'); ?>" onclick="insertTag('[u]','[/u]','<?= $view->textareaId; ?>');" /><br /><br />
+<input type="button" value="<?= Library_i18n::get('parser.buttons.underlined'); ?>" onclick="insertTag('[u]','[/u]','<?= $view->textareaId; ?>');" />
+<input type="button" value="<?= Library_i18n::get('parser.buttons.quote'); ?>" onclick="insertTag('[quote]','[/quote]','<?= $view->textareaId; ?>');" /><br /><br />
 <?= $view->tpl_smileys; ?>
