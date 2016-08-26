aa0bd2c33466963eb57ba3dbfaccdfacffb8776a
Tatane58 <antoine.charnotet@hotmail.fr>
2016-08-27 01:54:28 +0200

    Fix the file removing feat

--- application/models/Files.php
+++ application/models/Files.php
@@ -46,6 +46,59 @@
 				'sub_date' => 'DATE',
 			];
 		}
+
+		public function getUser()
+		{
+			return $this->load('user');
+		}
+
+		public function getChildren()
+		{
+			if( ! $this->is_dir)
+				return [];
+
+			return self::createRequest()
+					->where('parent_file.id=?', [$this->getId()])
+					->exec();
+		}
+		
+		public function getParentFile()
+		{
+			return $this->load('parent_file');
+		}
+		
+		public function getParentFileId()
+		{
+			$request = Model_Files::createRequest();
+			$results = $request->select('parent_file.id')
+							   ->where('id=?', [$this->getId()])
+							   ->getOnly(1)
+							   ->exec();
+
+			return empty($results->parent_file_id) ? null : $results->parent_file_id;
+		}
+		
+		public function getComments()
+		{
+			if($this->is_dir)
+				return [];
+
+			$request = Model_Comments::createRequest();
+			$results = $request->where('file.id=?', [$this->getId()])
+							   ->exec();
+			return $results;
+		}
+		
+		public function getViews()
+		{
+			if($this->is_dir)
+				return [];
+
+			$request = Model_Views::createRequest();
+			$results = $request->where('document.id=?', [$this->getId()])
+							   ->exec();
+			return $results;
+		}
 		
 		public function getPath()
 		{
@@ -213,33 +266,61 @@
 				$path	=	$this->getPath();
 
 				if( ! is_file($path))
-					throw new Exception(Library_i18n::get('spritecomics.delete.errors.not_found'), 404);
+				{
+					$this->response->error(Library_i18n::get('spritecomics.delete.file.errors.not_found'), 404);
+					return;
+				}
 
 				if( ! unlink($path))
-					throw new Exception(Library_i18n::get('spritecomics.delete.errors.unlink_failed'), 500);
+				{
+					$this->response->error(Library_i18n::get('spritecomics.delete.file.errors.unlink_failed'), 500);
+					return;
+				}
 
 				//And remove its thumbnail
 				$path	=	$this->getThumbPath();
 				
 				//Remove likes
-				$this->liked_users = [];
-				Model_Files::update($this);
+				$this->prop('liked_users', []);
 				
-				//And remove comments
+				//Remove comments
 				$comments	=	$this->getComments();
 
 				foreach($comments as $comment)
 					Model_Comments::delete($comment);
+				
+				//Remove views
+				$views = $this->getViews();
+
+				foreach($views as $view)
+					Model_Views::delete($view);
+					
+				//And remove tags
+				$this->prop('tags', []);
+
+				Model_Files::update($this);
 
 				if( ! is_file($path))
-					throw new Exception(Library_i18n::get('spritecomics.delete.errors.thumb_not_found'), 404);
+				{
+					$this->response->error(Library_i18n::get('spritecomics.delete.file.errors.thumb_not_found'), 404);
+					return;
+				}
 
 				if( ! unlink($path))
-					throw new Exception(Library_i18n::get('spritecomics.delete.errors.thumb_unlink_failed'), 500);
+				{
+					$this->response->error(Library_i18n::get('spritecomics.delete.file.errors.thumb_unlink_failed'), 500);
+					return;
+				}
 
 			}
 			else
 			{
+				//Remove tags
+				$this->prop('tags', []);
+
+				Model_Files::update($this);
+				
+				//And remove all children of this dir
 				$children	=	$this->getChildren();
 
 				foreach($children as $child)
@@ -247,37 +328,6 @@
 			}
 
 			self::delete($this);
-		}
-
-		public function getUser()
-		{
-			return $this->load('user');
-		}
-
-		public function getChildren()
-		{
-			if( ! $this->is_dir)
-				return [];
-
-			return self::createRequest()
-					->where('parent_file.id=?', [$this->getId()])
-					->exec();
-		}
-		
-		public function getParentFile()
-		{
-			return $this->load('parent_file');
-		}
-		
-		public function getParentFileId()
-		{
-			$request = Model_Files::createRequest();
-			$results = $request->select('parent_file.id')
-							   ->where('id=?', [$this->getId()])
-							   ->getOnly(1)
-							   ->exec();
-
-			return empty($results->parent_file_id) ? null : $results->parent_file_id;
 		}
 
 		public function isLikedByUser(Model_Users $user)
@@ -290,17 +340,6 @@
 		public function getNbrOfLikes()
 		{
 			return $this->load('liked_users')->count();
-		}
-		
-		public function getComments()
-		{
-			if($this->is_dir)
-				return [];
-
-			$request = Model_Comments::createRequest();
-			$results = $request->where('file.id=?', [$this->getId()])
-							   ->exec();
-			return $results;
 		}
 		
 		public static function search($string)
