714609f78535f27cbca25a61668cf2bb1fa3e6a9
Tatane58 <antoine.charnotet@hotmail.fr>
2016-08-27 16:48:16 +0200

    Max 255 chars per comment

--- public/javascript/spritecomics/gallery/moderation.js
+++ public/javascript/spritecomics/gallery/moderation.js
@@ -3,7 +3,7 @@ function turnCommentIntoForm(id, baseURL, content)
 	var formComment = "<form action=\"" + baseURL + "spritecomics/edit/comment\" method=\"post\" class=\"moderation\">";
 	formComment += "	<fieldset>";
 	formComment += "		<input type=\"hidden\" name=\"__method__\" value=\"update\" /><input type=\"hidden\" name=\"id\" value=\"" + id + "\" />";
-	formComment += "		<textarea name=\"content\">" + content + "</textarea>";
+	formComment += "		<textarea name=\"content\" maxlength=\"255\">" + content + "</textarea>";
 	formComment += "		<button><i class=\"fa fa-check-square-o\"></i></button>";
 	formComment += "	</fieldset>";
 	formComment += "</form>";
