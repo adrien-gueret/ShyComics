cfd14f1395976f56a60beca2ba9e209a44543f59
Tatane58 <antoine.charnotet@hotmail.fr>
2016-08-27 16:46:43 +0200

    Possibility to edit comments

---
+++ public/javascript/spritecomics/gallery/moderation.js
@@ -0,0 +1,11 @@
+function turnCommentIntoForm(id, baseURL, content)
+{
+	var formComment = "<form action=\"" + baseURL + "spritecomics/edit/comment\" method=\"post\" class=\"moderation\">";
+	formComment += "	<fieldset>";
+	formComment += "		<input type=\"hidden\" name=\"__method__\" value=\"update\" /><input type=\"hidden\" name=\"id\" value=\"" + id + "\" />";
+	formComment += "		<textarea name=\"content\">" + content + "</textarea>";
+	formComment += "		<button><i class=\"fa fa-check-square-o\"></i></button>";
+	formComment += "	</fieldset>";
+	formComment += "</form>";
+	document.getElementById('comment' + id).innerHTML = formComment;
+}
