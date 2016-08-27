0d6ad7cc3db9e757cc19f16e44d56a79fb5e4c24
Tatane58 <antoine.charnotet@hotmail.fr>
2016-08-27 18:19:56 +0200

    Possibility to edit tags

--- application/views/spritecomics/gallery/tags.php
+++ application/views/spritecomics/gallery/tags.php
@@ -1,12 +1,19 @@
 <div>
-	<?php if(!$view->is_index): ?>
-		<legend><i class="fa fa-tags"></i></legend>
-		<?php if(!$view->tags->isEmpty()): ?>
+	<legend><i class="fa fa-tags"></i></legend>
+	<div id="tagsDIV">
+		<?php if(!$view->is_index && !$view->tags->isEmpty()): ?>
 			<?php foreach($view->tags as $tag) : ?>
 				<?= $tag->prop('name'); ?> 
 			<?php endforeach; ?>
 		<?php else: ?>
 			<?= Library_i18n::get('spritecomics.gallery.details.no_tag'); ?>
+		<?php endif; ?>
+	</div>
+	<?php if(!$view->is_index && $view->can_edit): ?>
+		<?php if($view->tags->isEmpty()): ?>
+			<button onclick="turnTagsIntoForm(<?= $view->id; ?>, '<?= $view->base_url; ?>', true); return false;"><i class="fa fa-pencil-square-o"></i></button>
+		<?php else: ?>
+			<button onclick="turnTagsIntoForm(<?= $view->id; ?>, '<?= $view->base_url; ?>', false); return false;"><i class="fa fa-pencil-square-o"></i></button>
 		<?php endif; ?>
 	<?php endif; ?>
 </div>
