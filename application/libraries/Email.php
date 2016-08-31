f44d0562f7c94cdcfab9267bfd78ed7f3a6ba084
Tatane58 <antoine.charnotet@hotmail.fr>
2016-08-31 12:45:11 +0200

    Minor fixes

--- application/libraries/Email.php
+++ application/libraries/Email.php
@@ -1,12 +1,15 @@
 <?php
 	abstract class Library_Email
 	{
+		const MIME = "MIME-Version: 1.0\r\n",
+			  CONTENT_TYPE = "Content-Type: text/html; charset=UTF-8\r\n";
+		
 		public static function send($to, $subject, $message)
 		{
 			$expediteur = 'no-reply@shycomics.fr';
 			$headers = 'From: Shy\'Comics <' . $expediteur . '>' . "\r\n";
-			$headers .= "MIME-Version: 1.0\r\n";
-			$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+			$headers .= MIME;
+			$headers .= CONTENT_TYPE;
 
 			mail($to, $subject, $message, $headers);
 		}
@@ -15,8 +18,8 @@
 		{
 			$headers = 'From: ' . $pseudo . ' <' . $from . '>' . "\r\n";
 			$headers .= 'Reply-to: ' . $pseudo . ' <' . $from . '>' . "\r\n";
-			$headers .= "MIME-Version: 1.0\r\n";
-			$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+			$headers .= MIME;
+			$headers .= CONTENT_TYPE;
 
 			mail('Shylink <guignard.morgan@gmail.com>', $subject, $message, $headers);
 		}
