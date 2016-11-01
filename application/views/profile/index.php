b68f05e26656bf8f8bea8d6a8f2778dd6b6d9fc0
Tatane58 <antoine.charnotet@hotmail.fr>
2016-11-01 17:10:40 +0100

    Fix conflict

--- application/views/profile/index.php
+++ application/views/profile/index.php
@@ -11,7 +11,7 @@
 	<?php if(!$view->user_follows->isEmpty()): ?>
 		<?= Library_i18n::get('profile.index.follows'); ?>
 		<?php foreach($view->user_follows as $followed): ?>
-			<?= '<br /><img src="' . $followed->getAvatarURL() . '" alt="' . $followed->prop('username') . '" /><b>' . $followed->prop('username') . '</b> '?>
+			<?= '<a href="' . $followed->getId() . '"><img width="40px" height="40px" src="' . $followed->getAvatarURL() . '" alt="' . $followed->prop('username') . '" title="' . $followed->prop('username') . '" alt="' . $followed->prop('username') . '" /></a> '?>
 		<?php endforeach; ?>
 	<?php else: ?>
 		<?= Library_i18n::get('profile.index.no_follows'); ?>
