556f93ccc94dadeb35d99a52692da34536be7f08
Tatane58 <antoine.charnotet@hotmail.fr>
2017-11-04 13:06:42 +0100

    Bases of the new design

--- application/views/index/header.php
+++ application/views/index/header.php
@@ -8,69 +8,71 @@
 		<?= isset($view->social_NW_meta) ? $view->social_NW_meta : ''; ?>
 		<link rel="stylesheet" type="text/css" href="<?= $view->base_url; ?>public/css/style.css" />
 		<link rel="stylesheet" type="text/css" href="<?= $view->base_url; ?>public/css/font-awesome.min.css" />
-		<?= isset($view->additional_style) ? $view->additional_style : ''; ?>
+		<!--<?= isset($view->additional_style) ? $view->additional_style : ''; ?>-->
 		<link rel="shortcut icon" type="image/x-icon" href="<?= $view->base_url; ?>public/favicon.ico" />
 	</head>
 	<body>
 		<nav>
-			<a href="<?= $view->base_url; ?>"><img src="<?= $view->base_url; ?>public/images/logo_menu.png" title="<?= Library_i18n::get('index.header.navigation.helpers.home'); ?>" /></a>
-			<ul>
-				<li>
-					<?php if($view->current_member->isConnected()): ?>
-						<?= Library_i18n::get('index.header.welcome_name', $view->current_member->prop('username')); ?> &bull;
-						<a href="<?= $view->base_url; ?>profile" title="<?= Library_i18n::get('index.header.my_profile'); ?>">
-							<?= Library_i18n::get('index.header.my_profile'); ?>
-						</a> &bull;
-						<a href="<?= $view->base_url; ?>logout?token=<?= $_SESSION['token_logout']; ?>">
-							<?= Library_i18n::get('index.header.logout'); ?>
-						</a>
-					<?php else: ?>
-						<a href="<?= $view->base_url; ?>login/register">
-							<?= Library_i18n::get('index.header.register'); ?>
-						</a> |
-						<a href="<?= $view->base_url; ?>login">
-							<?= Library_i18n::get('index.header.connect'); ?>
-						</a>
-					<?php endif; ?>
-				</li>
-				<li>
-                    <form action="<?= $view->base_url; ?>search" method="post">
-                        <input type="search" name="search" id="nav-search"/>
-                        <input type="hidden" name="search_files" value="on" />
-                        <input type="hidden" name="search_dirs" value="on" />
-                        <input type="hidden" name="search_users" value="on" /> 
-                    </form>
-                </li>
-				<li>
-					<a href="<?= $view->base_url; ?>spritecomics">
-						<?= Library_i18n::get('index.header.navigation.spritecomics'); ?>
-					</a>
-				</li>
-				<li>
-					<a href="http://shycomics-forum.fr">
-						<?= Library_i18n::get('index.header.navigation.forum'); ?>
-					</a>
-				</li>
-				<li>
-					<a href="http://shyressources.shycomics.fr/">
-						<?= Library_i18n::get('index.header.navigation.shyresources'); ?>
-					</a>
-				</li>
-				<li>
-					<a href="https://www.facebook.com/pages/ShyComics/81657339196" class="fa-stack" title="<?= Library_i18n::get('index.header.navigation.helpers.facebook'); ?>">
-						<i class="fa fa-square-o fa-stack-2x"></i>
-						<i class="fa fa-facebook fa-stack-1x"></i>
-					</a>
-				</li>
-				<li>
-					<a href="https://twitter.com/ShyCoOfficiel" class="fa-stack" title="<?= Library_i18n::get('index.header.navigation.helpers.twitter'); ?>">
-						<i class="fa fa-square-o fa-stack-2x"></i>
-						<i class="fa fa-twitter fa-stack-1x"></i>
-					</a>
-				</li>
-			</ul>
-		</nav>
-		<nav>
+			<div>
+                <a href="<?= $view->base_url; ?>"><img src="<?= $view->base_url; ?>public/images/logoWhite.svg" alt="Shy'Comics" title="<?= Library_i18n::get('index.header.navigation.helpers.home'); ?>" /></a>
+                <!--<ul>
+                    <li>
+                        <?php if($view->current_member->isConnected()): ?>
+                            <?= Library_i18n::get('index.header.welcome_name', $view->current_member->prop('username')); ?> &bull;
+                            <a href="<?= $view->base_url; ?>profile" title="<?= Library_i18n::get('index.header.my_profile'); ?>">
+                                <?= Library_i18n::get('index.header.my_profile'); ?>
+                            </a> &bull;
+                            <a href="<?= $view->base_url; ?>logout?token=<?= $_SESSION['token_logout']; ?>">
+                                <?= Library_i18n::get('index.header.logout'); ?>
+                            </a>
+                        <?php else: ?>
+                            <a href="<?= $view->base_url; ?>login/register">
+                                <?= Library_i18n::get('index.header.register'); ?>
+                            </a> |
+                            <a href="<?= $view->base_url; ?>login">
+                                <?= Library_i18n::get('index.header.connect'); ?>
+                            </a>
+                        <?php endif; ?>
+                    </li>
+                    <li>
+                        <form action="<?= $view->base_url; ?>search" method="post">
+                            <input type="search" name="search" id="nav-search"/>
+                            <input type="hidden" name="search_files" value="on" />
+                            <input type="hidden" name="search_dirs" value="on" />
+                            <input type="hidden" name="search_users" value="on" /> 
+                        </form>
+                    </li>
+                    <li>
+                        <a href="<?= $view->base_url; ?>spritecomics">
+                            <?= Library_i18n::get('index.header.navigation.spritecomics'); ?>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://shycomics-forum.fr">
+                            <?= Library_i18n::get('index.header.navigation.forum'); ?>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://shyressources.shycomics.fr/">
+                            <?= Library_i18n::get('index.header.navigation.shyresources'); ?>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.facebook.com/pages/ShyComics/81657339196" class="fa-stack" title="<?= Library_i18n::get('index.header.navigation.helpers.facebook'); ?>">
+                            <i class="fa fa-square-o fa-stack-2x"></i>
+                            <i class="fa fa-facebook fa-stack-1x"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com/ShyCoOfficiel" class="fa-stack" title="<?= Library_i18n::get('index.header.navigation.helpers.twitter'); ?>">
+                            <i class="fa fa-square-o fa-stack-2x"></i>
+                            <i class="fa fa-twitter fa-stack-1x"></i>
+                        </a>
+                    </li>
+                </ul>-->
+            </div>
+        </nav>
+		<!--<nav>
 			<ul>
 				<?php if($view->current_member->isConnected()): ?>
 					<li>
@@ -103,16 +105,18 @@
 					</li>
 				<?php endif; ?>
 			</ul>
-		</nav>
+		</nav>-->
 		<header>
-			<img src="<?= $view->base_url; ?>public/images/logo_header.png" alt="Shy'Comics" />
-			<?php if(! $view->current_member->isConnected()): ?>
-				<div>
-					<a href="<?= $view->base_url; ?>login/register">
-						<?= Library_i18n::get('index.header.register_yourself') ;?>
-					</a>
-				</div>
-			<?php endif; ?>
+			<div>
+                <!--<?php if(! $view->current_member->isConnected()): ?>
+                    <div>
+                        <a href="<?= $view->base_url; ?>login/register">
+                            <?= Library_i18n::get('index.header.register_yourself') ;?>
+                        </a>
+                    </div>
+                <?php endif; ?>-->
+                Bandeau
+            </div>
 		</header>
 		<section>
 			<div class="main-container">
--- application/views/index/index.php
+++ application/views/index/index.php
@@ -1,38 +1,109 @@
-<div>
-    <div class="sub-container sub-container-a">
-        <h2><?= Library_i18n::get('index.index.title'); ?></h2>
-        <p>
-            <?= Library_i18n::get('index.index.presentation'); ?><br /><br />
-            <?= Library_i18n::get('index.index.update'); ?> <?= $view->news; ?>
-        </p>
+    <div class="grid">
+        <div class="cat">
+            <div>Catégorie</div>
+            <div class="legend">TOUT VOIR</div>
+        </div>
+        <div class="content">
+            <div class="element">
+                    <div></div><!--Necessary for puting the next div to the bottom-->
+                    <div>
+                        <div class="title">
+                            <div>
+                                <a href="#">
+                                    <div><b>Titre de SCsqdfqsdqsdqsdqsd</b></div>
+                                    <div>Page 1qsdqsdqsdqsdqsdqsdqsd</div>
+                                </a>
+                            </div>
+                            <div class="heart"><a href="rararar"><i class="fa fa-heart-o" aria-hidden="true"></i></a></div>
+                        </div>
+                    </div>
+            </div>
+            <div class="element"></div>
+            <div class="element"></div>
+            <div class="element"></div>
+            <div class="element"></div>
+            <div class="element"></div>
+        </div>
     </div>
+    <!--<div>
+        <div class="sub-container sub-container-a">
+            <h2><?= Library_i18n::get('index.index.title'); ?></h2>
+            <p>
+                <?= Library_i18n::get('index.index.presentation'); ?><br /><br />
+                <?= Library_i18n::get('index.index.update'); ?> <?= $view->news; ?>
+            </p>
+        </div>
 
-    <div class="sub-container sub-container-b">
-        <h2><?= Library_i18n::get('index.index.title_random'); ?></h2>
-        <div class="gallery"><?= $view->tpl_random; ?></div>
+        <div class="sub-container sub-container-b">
+            <h2><?= Library_i18n::get('index.index.title_random'); ?></h2>
+            <div class="gallery"><?= $view->tpl_random; ?></div>
+        </div>
     </div>
-</div>
-<div>
-    <div class="sub-container sub-container-c">
-        <h2><?= Library_i18n::get('index.index.title_last_uploads'); ?></h2>
-        <div class="gallery"><?= $view->tpl_last_boards; ?></div>
-    </div>
+    <div>
+        <div class="sub-container sub-container-c">
+            <h2><?= Library_i18n::get('index.index.title_last_uploads'); ?></h2>
+            <div class="gallery"><?= $view->tpl_last_boards; ?></div>
+        </div>
 
-    <div class="sub-container sub-container-d">
-        <h2><?= Library_i18n::get('index.index.title_last_comments'); ?></h2>
-        <p><?= $view->tpl_last_comments; ?></p>
+        <div class="sub-container sub-container-d">
+            <h2><?= Library_i18n::get('index.index.title_last_comments'); ?></h2>
+            <p><?= $view->tpl_last_comments; ?></p>
+        </div>
+    </div>
+    <div>
+        <div class="sub-container">
+            <h2><?= Library_i18n::get('index.index.title_populars'); ?></h2>
+            <div class="gallery"><?= $view->tpl_populars; ?></div>
+        </div>
+    </div>
+    <div>
+        <div class="sub-container">
+            <h2><?= Library_i18n::get('index.index.title_lost'); ?></h2>
+            <p><?= Library_i18n::get('index.index.desc_lost_1'); ?></p>
+            <p><?= Library_i18n::get('index.index.desc_lost_2', ['base_url' => $view->base_url]); ?></p>
+        </div>
+    </div>-->
+</div>
+<div class="featured">
+    <div>
+        <div>
+            <h1>En vedette</h1>
+            
+            Découvrez les meilleurs sprite-comics du moment, sélectionnés par l'équipe.
+            <br />
+            <br />
+            <a href="#" class="roundedWhite">TOUT VOIR</a>
+        </div>
+        <div></div><div></div><div></div>
     </div>
 </div>
-<div>
-    <div class="sub-container">
-        <h2><?= Library_i18n::get('index.index.title_populars'); ?></h2>
-        <div class="gallery"><?= $view->tpl_populars; ?></div>
+<div class="main-container">
+    <div class="grid">
+        <div class="cat">
+            <div>Catégorie</div>
+            <div class="legend">TOUT VOIR</div>
+        </div>
+        <div class="content">
+            <div class="element"></div>
+            <div class="element"></div>
+            <div class="element"></div>
+            <div class="element"></div>
+            <div class="element"></div>
+            <div class="element"></div>
+        </div>
     </div>
-</div>
-<div>
-    <div class="sub-container">
-        <h2><?= Library_i18n::get('index.index.title_lost'); ?></h2>
-        <p><?= Library_i18n::get('index.index.desc_lost_1'); ?></p>
-        <p><?= Library_i18n::get('index.index.desc_lost_2', ['base_url' => $view->base_url]); ?></p>
+    <div class="main-container">
+    <div class="grid">
+        <div class="cat">
+            <div>Catégorie</div>
+            <div class="legend">TOUT VOIR</div>
+        </div>
+        <div class="content">
+            <div class="element"></div>
+            <div class="element"></div>
+            <div class="element"></div>
+            <div class="element"></div>
+            <div class="element"></div>
+            <div class="element"></div>
+        </div>
     </div>
-</div>
